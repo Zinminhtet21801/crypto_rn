31d2007a52b0035956b5d482c1766939db7a6ac4
Zin Min Htet <zetminhtin@gmail.com>
2021-12-17 00:26:24 +0630

    added watchlist

--- App.js
+++ App.js
@@ -1,21 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import Constants from "expo-constants";
+import HomeScreen from "./src/Screens/HomeScreen";
+import CoinDetailsScreen from "./src/Screens/CoinDetailsScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import Navigation from "./src/navigation";
+import WatchListProvider from "./src/Contexts/WatchListContext";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer
+      theme={{
+        colors: {
+          background: "#121212",
+        },
+      }}
+    >
+      <WatchListProvider>
+        <View style={styles.container}>
+          <StatusBar style="light" />
+          <Navigation />
+        </View>
+      </WatchListProvider>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#121212",
+    paddingTop: Constants.statusBarHeight,
   },
 });
